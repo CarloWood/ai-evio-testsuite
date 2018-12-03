8265482eb87d650bac81adc70c8fdce7cd35cfd8
Carlo Wood <carlo@alinoe.com>
2018-12-03 19:22:13 +0100

    Don't require a port number.

--- src/arpa.cxx
+++ src/arpa.cxx
@@ -4,13 +4,19 @@
 #include "utils/AIAlert.h"
 #include "utils/debug_ostream_operators.h"
 
-int main(int, char* argv[])
+int main(int argc, char* argv[])
 {
   Debug(NAMESPACE_DEBUG::init());
 
+  if (argc != 2)
+  {
+    std::cerr << "Usage: " << argv[0] << ' ' << "<IP-number>" << std::endl;
+    return 1;
+  }
+
   try
   {
-    evio::SocketAddress sa(argv[1]);
+    evio::SocketAddress sa(argv[1], 0);
 
     evio::SocketAddress::arpa_buf_t arpa_buf;
     sa.ptr_qname(arpa_buf);
