db5f2fd71c59fb9488c478098b96e36641d934f1
Carlo Wood <carlo@alinoe.com>
2020-06-15 19:57:47 +0200

    Make listen_socket work.

--- src/listen_socket.cxx
+++ src/listen_socket.cxx
@@ -19,6 +19,36 @@ using evio::SocketAddress;
 using evio::GetThread;
 template<threadpool::Timer::time_point::rep count, typename Unit> using Interval = threadpool::Interval<count, Unit>;
 
+class MyAcceptedSocketDecoder : public InputDecoder
+{
+ private:
+  size_t m_received;
+
+ public:
+  MyAcceptedSocketDecoder() : m_received(0) { }
+
+ protected:
+  size_t end_of_msg_finder(char const* new_data, size_t rlen) override { return rlen; }
+  void decode(int& allow_deletion_count, MsgBlock&& msg) override;
+};
+
+using MyAcceptedSocket = evio::AcceptedSocket<MyAcceptedSocketDecoder, OutputStream>;
+
+class MyListenSocket : public evio::ListenSocket<MyAcceptedSocket>
+{
+ public:
+  // Called when a new connection is accepted.
+  void new_connection(accepted_socket_type& accepted_socket) override
+  {
+    Dout(dc::notice, "New connection to listen socket was accepted. Sending 10000 bytes of data.");
+    // Write 10 kbyte of data.
+    for (int n = 0; n < 100; ++n)
+      accepted_socket() << "START012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789END." << std::endl;
+    // We're done with writing to this socket.
+    accepted_socket.flush_output_device();
+  }
+};
+
 class MyDecoder : public InputDecoder
 {
  private:
@@ -31,31 +61,16 @@ class MyDecoder : public InputDecoder
   void decode(int& allow_deletion_count, MsgBlock&& msg) override;
 };
 
-using MyAcceptedSocket = evio::AcceptedSocket<MyDecoder, OutputStream>;
-
-class MyListenSocket : public evio::ListenSocket<MyAcceptedSocket>
-{
- public:
-  // Called when a new connection is accepted.
-  void new_connection(accepted_socket_type& accepted_socket) override
-  {
-    Dout(dc::notice, "New connection to listen socket was accepted. Sending 10000 bytes of data.");
-    // Write 10 kbyte of data.
-    for (int n = 0; n < 100; ++n)
-      accepted_socket() << "START012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789END." << std::endl;
-  }
-};
-
 class BurstSocket : public evio::Socket
 {
   private:
-//   MyDecoder m_input;
+   MyDecoder m_input;
    OutputStream m_output;
 
   public:
    BurstSocket()
    {
-//     set_sink(m_input);
+     set_sink(m_input);
      set_source(m_output);
    }
 
@@ -80,19 +95,23 @@ int main()
 
   try
   {
+    // This timer fires while we're in the destructor of event_loop.
+    // Therefore it must be constructed before event_loop, or it would
+    // be cancelled before it expires.
+    threadpool::Timer timer;
+
     // Initialize the IO event loop thread.
     evio::EventLoop event_loop(low_priority_handler);
 
     // Start a listen socket on port 9001 that is closed after 10 seconds.
-    threadpool::Timer timer;
     {
       auto listen_sock = evio::create<MyListenSocket>();
       listen_sock->listen(listen_address);
       timer.start(Interval<10, std::chrono::seconds>(),
           [&timer, listen_sock]()
           {
-            timer.release_callback();
             listen_sock->close();
+            timer.release_callback();   // This destroys `listen_sock` (the copy inside this lambda).
             evio::EventLoopThread::instance().bump_terminate();
           });
     }
@@ -118,7 +137,7 @@ int main()
       for (size_t s = 0; s < sockets.size(); ++s)
       {
         sockets[s]->write_burst();
-        //sockets[s]->flush_output_device();
+        sockets[s]->flush_output_device();
       }
     }
 
@@ -132,12 +151,22 @@ int main()
   Dout(dc::notice, "Leaving main...");
 }
 
-void MyDecoder::decode(int& CWDEBUG_ONLY(allow_deletion_count), MsgBlock&& msg)
+void MyAcceptedSocketDecoder::decode(int& allow_deletion_count, MsgBlock&& msg)
+{
+  // Just print what was received.
+  DoutEntering(dc::notice, "MyAcceptedDecoder::decode({" << allow_deletion_count << "}, \"" << buf2str(msg.get_start(), msg.get_size()) << "\") [" << this << ']');
+  m_received += msg.get_size();
+  // Close the socket when the last message was received.
+  if (m_received == 22) // Two times "Burst data!".
+    close_input_device(allow_deletion_count);
+}
+
+void MyDecoder::decode(int& allow_deletion_count, MsgBlock&& msg)
 {
   // Just print what was received.
   DoutEntering(dc::notice, "MyDecoder::decode({" << allow_deletion_count << "}, \"" << buf2str(msg.get_start(), msg.get_size()) << "\") [" << this << ']');
   m_received += msg.get_size();
   // Stop when the last message was received.
   if (m_received == 10000)
-    stop_input_device();
+    close_input_device(allow_deletion_count);
 }
