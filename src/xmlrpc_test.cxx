cab7266eacb09c4ebdba92272ac7d61994c02285
Carlo Wood <carlo@alinoe.com>
2021-02-14 10:38:51 +0100

    Add xmlrpc_test.cxx. Add magic_enum to gitache (now needed by evio).

---
+++ src/xmlrpc_test.cxx
@@ -0,0 +1,28 @@
+#include "sys.h"
+#include "evio/protocol/xmlrpc/initialize.h"
+#include "utils/AIAlert.h"
+#include "utils/debug_ostream_operators.h"
+#include "debug.h"
+
+int main()
+{
+  Debug(debug::init());
+
+  std::string base64_str = "SGVsbG8gd29ybGQ=";
+  std::string iso8601_str = "2021-01-29T15:37:23Z";
+
+  try
+  {
+    evio::BinaryData b1;
+    xmlrpc::initialize(b1, base64_str);
+    Dout(dc::notice, "b1 = " << b1);
+
+    evio::DateTime d1;
+    xmlrpc::initialize(d1, iso8601_str);
+    Dout(dc::notice, "d1 = " << d1);
+  }
+  catch (AIAlert::Error const& error)
+  {
+    Dout(dc::warning, error);
+  }
+}
